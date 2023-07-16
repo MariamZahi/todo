cf43788fe88ebaa9ca97dbac186eba3bb209e5c1
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-07-16 03:36:40 -0400

    layout defined

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.todo
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
+
+    // ... (other code)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ... (other code)
+
+        listViewTasks.onItemClickListener = AdapterView.OnItemClickListener { _, view, position, _ ->
+            val task = taskList[position]
+            taskList.removeAt(position)
+            adapter.notifyDataSetChanged()
+            Toast.makeText(this, "Completed: $task", Toast.LENGTH_SHORT).show()
+        }
+
+        // Add checkbox listener to mark tasks as done
+        listViewTasks.setOnCheckedChangeListener { _, position, isChecked ->
+            val task = taskList[position]
+            if (isChecked) {
+                taskList[position] = "✓ $task"
+            } else {
+                taskList[position] = task.removePrefix("✓ ")
+            }
+            adapter.notifyDataSetChanged()
+        }
     }
 }
